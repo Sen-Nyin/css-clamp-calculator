5855c82f01d7d55f7a450b6ce2f0a0622023021a
Leon Lonsdale <leonlonsdale@icloud.com>
2022-10-15 13:21:32 +0100

    functions moved to iife, favicon added

--- scripts.js
+++ scripts.js
@@ -1,62 +1,64 @@
 'use: strict';
 
-const findEle = (selector) => document.querySelector(selector);
+(() => {
+  const findEle = (selector) => document.querySelector(selector);
 
-const calcVP = (value) => {
-  const rem = findEle('[data-rem-value]').value || 16;
-  const result = value / rem;
-  return result;
-};
+  const calcVP = (value) => {
+    const rem = findEle('[data-rem-value]').value || 16;
+    const result = value / rem;
+    return result;
+  };
 
-const elements = {
-  calculate: findEle('[data-btn-calculate]'),
-  result: findEle('[data-result]'),
-  copy: findEle('[data-copy]'),
-  reset: findEle('[data-btn-reset]'),
-};
+  const elements = {
+    calculate: findEle('[data-btn-calculate]'),
+    result: findEle('[data-result]'),
+    copy: findEle('[data-copy]'),
+    reset: findEle('[data-btn-reset]'),
+  };
 
-const calcSlope = (min, max, minVP, maxVP) => {
-  const sizeGap = max - min;
-  const vpGap = maxVP - minVP;
-  const slope = sizeGap / vpGap;
-  return slope;
-};
+  const calcSlope = (min, max, minVP, maxVP) => {
+    const sizeGap = max - min;
+    const vpGap = maxVP - minVP;
+    const slope = sizeGap / vpGap;
+    return slope;
+  };
 
-const calcYIntersect = (minVP, slope, minimum) => {
-  const yIntersect = (0 - minVP) * slope + minimum;
-  return yIntersect;
-};
+  const calcYIntersect = (minVP, slope, minimum) => {
+    const yIntersect = (0 - minVP) * slope + minimum;
+    return yIntersect;
+  };
 
-const displayResult = (min, yInt, slope, max) => {
-  const result = `clamp(${min}rem, ${yInt}rem + ${slope * 100}vw, ${max}rem)`;
-  elements.result.textContent = result;
-};
+  const displayResult = (min, yInt, slope, max) => {
+    const result = `clamp(${min}rem, ${yInt}rem + ${slope * 100}vw, ${max}rem)`;
+    elements.result.textContent = result;
+  };
 
-const handleCalculate = (e) => {
-  e.preventDefault();
-  const maximum = +findEle('[data-size-max]').value;
-  const minimum = +findEle('[data-size-min]').value;
-  const maximumVP = calcVP(+findEle('[data-vp-max]').value);
-  const minimumVP = calcVP(+findEle('[data-vp-min]').value);
-  const slope = calcSlope(minimum, maximum, minimumVP, maximumVP);
-  const yIntersect = calcYIntersect(minimumVP, slope, minimum);
-  displayResult(minimum, yIntersect, slope, maximum);
-};
+  const handleCalculate = (e) => {
+    e.preventDefault();
+    const maximum = +findEle('[data-size-max]').value;
+    const minimum = +findEle('[data-size-min]').value;
+    const maximumVP = calcVP(+findEle('[data-vp-max]').value);
+    const minimumVP = calcVP(+findEle('[data-vp-min]').value);
+    const slope = calcSlope(minimum, maximum, minimumVP, maximumVP);
+    const yIntersect = calcYIntersect(minimumVP, slope, minimum);
+    displayResult(minimum, yIntersect, slope, maximum);
+  };
 
-const handleCopy = (e) => {
-  e.preventDefault();
-  const copyText = elements.result.textContent;
-  if (!copyText) return;
-  navigator.clipboard.writeText(copyText);
-};
+  const handleCopy = (e) => {
+    e.preventDefault();
+    const copyText = elements.result.textContent;
+    if (!copyText) return;
+    navigator.clipboard.writeText(copyText);
+  };
 
-const handleReset = (e) => {
-  e.preventDefault();
-  const form = findEle('[data-form]');
-  form.reset();
-  elements.result.textContent = '';
-};
+  const handleReset = (e) => {
+    e.preventDefault();
+    const form = findEle('[data-form]');
+    form.reset();
+    elements.result.textContent = '';
+  };
 
-elements.calculate.addEventListener('click', handleCalculate);
-elements.copy.addEventListener('click', handleCopy);
-elements.reset.addEventListener('click', handleReset);
+  elements.calculate.addEventListener('click', handleCalculate);
+  elements.copy.addEventListener('click', handleCopy);
+  elements.reset.addEventListener('click', handleReset);
+})();
