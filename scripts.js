f0a143144f7b178d4b4a09c6bce88998c8d4f255
Leon Lonsdale <leonlonsdale@icloud.com>
2022-10-21 09:27:40 +0100

    froze elements object

--- scripts.js
+++ scripts.js
@@ -1,70 +1,60 @@
-'use: strict';
-
-(() => {
-  const findEle = (selector) => document.querySelector(selector);
-
-  const elements = Object.freeze({
+"use strict";
+const findEle = (selector) => document.querySelector(selector);
+const elements = Object.freeze({
     calculate: findEle('[data-btn-calculate]'),
     result: findEle('[data-result]'),
     copy: findEle('[data-copy]'),
     reset: findEle('[data-btn-reset]'),
-  });
-
-  const subtract = (num1, num2) => num1 - num2;
-  const divide = (num1, num2) => num1 / num2;
-
-  const calcVP = (pixels) => {
-    const pixelsPerRem = findEle('[data-rem-value]').value || 16;
+});
+const subtract = (num1, num2) => num1 - num2;
+const divide = (num1, num2) => num1 / num2;
+const calcVP = (pixels) => {
+    const pixelsPerRemElement = findEle('[data-rem-value]');
+    const pixelsPerRem = +pixelsPerRemElement.value;
     const rem = divide(pixels, pixelsPerRem);
     return rem;
-  };
-
-  const calcSlope = (sizeDif, vpDif) => {
+};
+const calcSlope = (sizeDif, vpDif) => {
     const slope = divide(sizeDif, vpDif);
     return slope;
-  };
-
-  const calcYIntersect = (minVP, slope, minimum) => {
+};
+const calcYIntersect = (minVP, slope, minimum) => {
     const yIntersect = (0 - minVP) * slope + minimum;
     return yIntersect;
-  };
-
-  const renderResult = (min, yInt, slope, max) => {
+};
+const renderResult = (min, yInt, slope, max) => {
     const result = `clamp(${min}rem, ${yInt}rem + ${slope * 100}vw, ${max}rem)`;
     elements.result.textContent = result;
-  };
-
-  const handleCalculate = (e) => {
-    e.preventDefault();
-    const maximumSize = +findEle('[data-size-max]').value;
-    const minimumSize = +findEle('[data-size-min]').value;
-    const maximumVP = calcVP(+findEle('[data-vp-max]').value);
-    const minimumVP = calcVP(+findEle('[data-vp-min]').value);
-
-    const sizeDif = subtract(maximumSize, minimumSize);
-    const vpDif = subtract(maximumVP, minimumVP);
-
-    const slope = calcSlope(sizeDif, vpDif);
-    const yIntersect = calcYIntersect(minimumVP, slope, minimumSize);
-
-    renderResult(minimumSize, yIntersect, slope, maximumSize);
-  };
-
-  const handleCopy = (e) => {
+};
+const handleCopy = (e) => {
     e.preventDefault();
     const copyText = elements.result.textContent;
-    if (!copyText) return;
+    if (!copyText)
+        return;
     navigator.clipboard.writeText(copyText);
-  };
-
-  const handleReset = (e) => {
+};
+const handleCalculate = (e) => {
+    e.preventDefault();
+    const maxSizeInput = findEle('[data-size-max]');
+    const minSizeInput = findEle('[data-size-min]');
+    const maxVPInput = findEle('[data-vp-max]');
+    const minVPInput = findEle('[data-vp-min]');
+    const maximumSize = +maxSizeInput.value;
+    const minimumSize = +minSizeInput.value;
+    const maximumVP = +maxVPInput.value;
+    const minimumVP = +minVPInput.value;
+    const sizeDif = subtract(maximumSize, minimumSize);
+    const vpDif = subtract(maximumVP, minimumVP);
+    const slope = calcSlope(sizeDif, vpDif);
+    const yIntersect = calcYIntersect(minimumVP, slope, minimumSize);
+    renderResult(minimumSize, yIntersect, slope, maximumSize);
+};
+const handleReset = (e) => {
     e.preventDefault();
     const form = findEle('[data-form]');
     form.reset();
     elements.result.textContent = '';
-  };
-
-  elements.calculate.addEventListener('click', handleCalculate);
-  elements.copy.addEventListener('click', handleCopy);
-  elements.reset.addEventListener('click', handleReset);
-})();
+};
+elements.calculate.addEventListener('click', handleCalculate);
+elements.copy.addEventListener('click', handleCopy);
+elements.reset.addEventListener('click', handleReset);
---
+++ ts-files/scripts.ts
@@ -0,0 +1,80 @@
+const findEle = (selector: string): HTMLElement => document.querySelector(selector) as HTMLElement;
+
+interface Elements {
+  calculate: HTMLButtonElement;
+  result: HTMLElement;
+  copy: HTMLButtonElement;
+  reset: HTMLButtonElement;
+}
+
+const elements: Elements = Object.freeze({
+  calculate: findEle('[data-btn-calculate]') as HTMLButtonElement,
+  result: findEle('[data-result]') as HTMLElement,
+  copy: findEle('[data-copy]') as HTMLButtonElement,
+  reset: findEle('[data-btn-reset]') as HTMLButtonElement,
+});
+
+const subtract = (num1: number, num2: number): number => num1 - num2;
+const divide = (num1: number, num2: number): number => num1 / num2;
+
+const calcVP = (pixels: number): number => {
+  const pixelsPerRemElement: HTMLInputElement = findEle('[data-rem-value]') as HTMLInputElement;
+  const pixelsPerRem: number = +pixelsPerRemElement.value;
+  const rem: number = divide(pixels, pixelsPerRem);
+  return rem;
+};
+
+const calcSlope = (sizeDif: number, vpDif: number): number => {
+  const slope: number = divide(sizeDif, vpDif);
+  return slope;
+};
+
+const calcYIntersect = (minVP: number, slope: number, minimum: number): number => {
+  const yIntersect = (0 - minVP) * slope + minimum;
+  return yIntersect;
+};
+
+const renderResult = (min: number, yInt: number, slope: number, max: number): void => {
+  const result = `clamp(${min}rem, ${yInt}rem + ${slope * 100}vw, ${max}rem)`;
+  elements.result.textContent = result;
+};
+
+const handleCopy = (e: Event) => {
+  e.preventDefault();
+  const copyText = elements.result.textContent;
+  if (!copyText) return;
+  navigator.clipboard.writeText(copyText);
+};
+
+const handleCalculate = (e: Event) => {
+  e.preventDefault();
+
+  const maxSizeInput: HTMLInputElement = findEle('[data-size-max]') as HTMLInputElement;
+  const minSizeInput: HTMLInputElement = findEle('[data-size-min]') as HTMLInputElement;
+  const maxVPInput: HTMLInputElement = findEle('[data-vp-max]') as HTMLInputElement;
+  const minVPInput: HTMLInputElement = findEle('[data-vp-min]') as HTMLInputElement;
+
+  const maximumSize: number = +maxSizeInput.value;
+  const minimumSize: number = +minSizeInput.value;
+  const maximumVP: number = +maxVPInput.value;
+  const minimumVP: number = +minVPInput.value;
+
+  const sizeDif: number = subtract(maximumSize, minimumSize);
+  const vpDif: number = subtract(maximumVP, minimumVP);
+
+  const slope: number = calcSlope(sizeDif, vpDif);
+  const yIntersect: number = calcYIntersect(minimumVP, slope, minimumSize);
+
+  renderResult(minimumSize, yIntersect, slope, maximumSize);
+};
+
+const handleReset = (e: Event) => {
+  e.preventDefault();
+  const form: HTMLFormElement = findEle('[data-form]') as HTMLFormElement;
+  form.reset();
+  elements.result.textContent = '';
+};
+
+elements.calculate.addEventListener('click', handleCalculate);
+elements.copy.addEventListener('click', handleCopy);
+elements.reset.addEventListener('click', handleReset);
